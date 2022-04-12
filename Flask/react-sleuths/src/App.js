1761bcdb3fe6c84fd8cae846b902b45c4390d158
Patricia Booth <44758340+alpacawool@users.noreply.github.com>
2022-04-12 14:25:13 -0700

    Added planned react routes and base page code
    Added 4 main react-router routes: Index redirects to All Projects page, Login page, Projects shows All Projects, Projects/new creates a new project, Projects/#ID shows a single project. Created base React components along with SASS css files for each page in the pages directory. Installed npm packages - react-router-dom, sass, and material packages. Configured app.py to use react routes.

--- Flask/react-sleuths/src/App.js
+++ Flask/react-sleuths/src/App.js
@@ -1,10 +1,16 @@
-import {TestComponent} from './components/TestComponent/TestComponent'
 import {useState, useEffect} from 'react'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import AllProjects from './pages/AllProjects/AllProjects';
+import SingleProject from './pages/SingleProject/SingleProject';
+import Login from './pages/Login/Login';
+import NewProject from './pages/NewProject/NewProject';
+
 
 function App() {
 
   const [state, setState] = useState({})
 
+  // Example of using useEffect to get JSON data from Flask
   useEffect(() => {
     fetch("/hello").then(response => {
       if (response.status === 200) {
@@ -16,7 +22,25 @@ function App() {
 
   return (
     <div className="App">
-      <TestComponent prop={state}/>
+      {/* Frontend Routes using react-router */}
+      <BrowserRouter>
+        <Routes>
+          {/* Home page - Show all projects 
+              TODO: Show login page if NOT logged in*/}
+          <Route path="/">
+            <Route index element={<AllProjects />} />
+          </Route>
+          {/* Login page
+              TODO: User Authentication handling */}
+          <Route path="login" element={<Login />} />
+          {/* Project URL handling */}
+          <Route path="projects">
+            <Route index element = {<AllProjects />} />
+            <Route path =":projectid" element={<SingleProject />} />
+            <Route path="new" element={<NewProject />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
